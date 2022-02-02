304c71b9e96d084ff528fe0e02a60bcdb04dd4da
Vyacheslav Pogorelskiy <vyacheslav.pogorelskiy@gmail.com>
2022-02-02 11:04:38 +0300

    Created project

---
+++ Shared/OTUS_Homework_9App.swift
@@ -0,0 +1,17 @@
+//
+//  OTUS_Homework_9App.swift
+//  Shared
+//
+//  Created by Вячеслав Погорельский on 02.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct OTUS_Homework_9App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
